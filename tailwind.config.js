e01be462f2c53aae4947cb7431dfd44ed4b32062
Jo <joniqueeleda@gmail.com>
2024-07-09 20:22:22 +0800

    Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playFair: ["Playfair Display", 'serif']
+      },
+      colors: {
+        lightBeige: ["#F2EAD3"],
+        darkBeige: ["#DFD7BF"],
+        darkBrown: ["#3F2305"],
+        dirtyWhite: ["#F3EEEA"]
+      }
+    },
+  },
+  plugins: [],
+}
+
